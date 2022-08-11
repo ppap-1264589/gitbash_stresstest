2fc783e365cd99bd11f743f881cb491942969408
ppap-1264589 <88699088+ppap-1264589@users.noreply.github.com>
2022-08-11 10:51:37 +0700

    Update correct.cpp - VOMARIO.cpp

--- correct.cpp
+++ correct.cpp
@@ -6,25 +6,32 @@
 #define int long long
 #define all(x) x.begin(), x.end()
 #define purge(x) x.resize(unique(x.begin(), x.end()) - x.begin())
+#define ep emplace_back
 using namespace std;
 
 const int maxn = 1e5 + 10;
-pii Line[maxn];
 
 int n;
 int x[maxn], w[maxn], e[maxn];
-pii T[maxn << 2]; // IT Doan Thang
 
 int realval[maxn];
-int get(const pii& L, int x){
-    return L.a*realval[x] + L.b;
-}
+struct LINE{
+    int a,b;
+    int id;
+    int get(const int& x){
+        return a*realval[x] + b;
+    }
+};
+LINE T[maxn << 2]; // IT Doan Thang
 
-int query(int nod, int l, int r, int pos){
-    int res = get(T[nod], pos);
+
+bitset<maxn << 2> nothing;
+pii query(int nod, int l, int r, int pos){
+    pii res;
+    res = {T[nod].get(pos), T[nod].id};
     if (l == r) return res;
 
-    int val;
+    pii val;
     int mid = (l+r) >> 1;
 
     if (pos <= mid) val = query(nod*2, l, mid, pos);
@@ -33,19 +40,23 @@ int query(int nod, int l, int r, int pos){
     return res;
 }
 
-void update(int nod, int l, int r, int u, int v, const pii& Line){
+void update(int nod, int l, int r, int u, int v, LINE Line){
     if (l > v || r < u) return;
 
     int mid = (l+r) >> 1;
     if (l >= u && r <= v){
-        int TL = get(T[nod], l);
-        int Tmid1 = get(T[nod], mid);
-        int Tmid2 = get(T[nod], mid+1);
-        int TR = get(T[nod], r);
-        int LineL = get(Line, l);
-        int Linemid1 = get(Line, mid);
-        int Linemid2 = get(Line, mid+1);
-        int LineR = get(Line, r);
+        int id1 = T[nod].id;
+        int id2 = Line.id;
+        pii TL = {T[nod].get(l), id1};
+        pii TR = {T[nod].get(r), id1};
+        pii LineL = {Line.get(l), id2};
+        pii LineR = {Line.get(r), id2};
+
+        if (nothing[nod]){
+            nothing[nod] = 0;
+            T[nod] = Line;
+            return;
+        }
 
         if (TL >= LineL && TR >= LineR){
             return;
@@ -54,29 +65,6 @@ void update(int nod, int l, int r, int u, int v, const pii& Line){
             T[nod] = Line;
             return;
         }
-
-        if (TL <= LineL && Tmid1 <= Linemid1){
-            update(nod*2+1, mid+1, r, u, v, T[nod]);
-            T[nod] = Line;
-            return;
-        }
-
-        if (TL >= LineL && Tmid1 >= Linemid1){
-            update(nod*2+1, mid+1, r, u, v, Line);
-            return;
-        }
-
-        if (Tmid2 <= Linemid2 && TR <= LineR){
-            update(nod*2, l, mid, u, v, T[nod]);
-            T[nod] = Line;
-            return;
-        }
-
-        if (Tmid2 >= Linemid2 && TR >= LineR){
-            update(nod*2, l, mid, u, v, Line);
-            return;
-        }
-        return;
     }
 
     update(nod*2, l, mid, u, v, Line);
@@ -85,10 +73,10 @@ void update(int nod, int l, int r, int u, int v, const pii& Line){
 
 vector<int> X; // Vector Roi rac hoa
 int SIZE;
-void update(int l, int r, const pii& Line){
+void update(int l, int r, const LINE& Line){
     update(1, 1, SIZE, l, r, Line);
 }
-int query(int pos){
+pii query(int pos){
     return query(1, 1, SIZE, pos);
 }
 int LB(const int& val){
@@ -99,12 +87,14 @@ int UB(const int& val){
 }
 
 int f[maxn];
+int tr[maxn];
 void DP(){
-    int maxx = 0;
     up(i,1,n){
         int cur = LB(x[i]);
-        f[i] = query(cur) + e[i];
-        if (w[i] == 0) update(1, SIZE, {0ll, f[i]});
+        auto found = query(cur);
+        f[i] = found.first + e[i];
+        tr[i] = found.second;
+        if (w[i] == 0) update(1, SIZE, {0ll, f[i], i});
         // never mess with binary search !!
         // this update mean "insert a line from -oo to oo with a = 0 and b = f[i]"
         // 0 = lower_bound(-oo);
@@ -112,12 +102,10 @@ void DP(){
         else{
             int l = x[i] - f[i]/w[i];
             int r = x[i] + f[i]/w[i];
-            update(LB(l), cur, {w[i], f[i] - w[i]*x[i]});
-            update(cur, UB(r), {-w[i], f[i] + w[i]*x[i]});
+            update(LB(l), cur, {w[i], f[i] - w[i]*x[i], i});
+            update(cur, UB(r), {-w[i], f[i] + w[i]*x[i], i});
         }
-        maxx = max(maxx, f[i]);
     }
-    cout << maxx;
 }
 
 signed main()  {
@@ -138,17 +126,29 @@ signed main()  {
     sort(all(X));
     purge(X);
     SIZE = X.size();
+    nothing.set();
 
     X.insert(X.begin(), -1e9 - 7);
     X.push_back(1e9 + 7);
     up(i,1,SIZE) realval[i] = X[i];
 
     DP();
+
+    int last = -1;
+    int maxx = 0;
+    up(i,1,n){
+        if (maxx < f[i]){
+            maxx = f[i];
+            last = i;
+        }
+    }
+    cout << maxx << "\n";
+
+//    vector<int> path;
+//    while (last != 0){
+//        path.ep(last);
+//        last = tr[last];
+//    }
+//    reverse(all(path));
+//    for (auto x : path) cout << x << " ";
 }
-
-//solution for https://oj.vnoi.info/problem/vomario
-
-/*
-holy shit this is how you break the DP formular
-//https://vnoi.info/forum/11/5384/?page=2
-*/
