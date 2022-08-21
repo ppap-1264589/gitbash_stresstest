0cefe05e89b644f7d39056dddd4ed3fefa140f46
ppap-1264589 <88699088+ppap-1264589@users.noreply.github.com>
2022-08-22 00:06:28 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,17 +4,12 @@ g++ generator.cpp -o generator
 g++ correct.cpp -o correct
 
 for((i = 1; ; ++i)); do
-    ./generator $i > input_file
-    ./my < input_file > myAnswer
-    ./correct < input_file > correctAnswer
+    ./generator $i > input_file.inp
+    ./my < input_file.inp > myAnswer.out
+    ./correct < input_file.inp > correctAnswer.out
 
-    diff -Z myAnswer correctAnswer > /dev/null || break
-    echo "Passed: " $i
+    diff -Z myAnswer.out correctAnswer.out > /dev/null || break
+    echo "Passed:" $i
 done
 
 echo "WA !"
-
-echo "Your answer is:"
-cat myAnswer
-echo "Correct answer is:"
-cat correctAnswer
